e1d64239c326d7c3500eabd75f033ad969502298
Mehrdad Zade <mehrdad.azh@gmail.com>
2020-11-07 22:45:49 -0500

    a

---
+++ Alert/app/src/main/java/com/example/alert/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.alert;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you sure?")
+                .setMessage("You are about to delete this. Do you want to proceed?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "It's Done!", Toast.LENGTH_SHORT).show();
+                        Log.i("Info", "Yes was pressed.............................");
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+    }
+}
