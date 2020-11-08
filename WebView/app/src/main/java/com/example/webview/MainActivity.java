35a78319da46110b240518e1ceb11aa518e89d16
Mehrdad Zade <mehrdad.azh@gmail.com>
2020-11-07 22:47:01 -0500

    ww

---
+++ WebView/app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());//this argument makes sure the web content is opened within the app. if not provided default browser will kick in
+        webView.loadUrl("https://finance.yahoo.com");
+
+    }
+}
